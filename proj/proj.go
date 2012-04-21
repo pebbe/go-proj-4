0605bf27e74b2f873d03409ddbb9e0e30a2a7782
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-04-21 14:34:50 +0200

    simplify

--- proj/proj.go
+++ proj/proj.go
@@ -5,23 +5,23 @@ See: http://trac.osgeo.org/proj/
 
 Example usage:
 
-	merc, err := NewProj("+proj=merc +ellps=clrk66 +lat_ts=33")
-	defer merc.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+    merc, err := NewProj("+proj=merc +ellps=clrk66 +lat_ts=33")
+    defer merc.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	ll, err := NewProj("+proj=latlong +ellps=clrk66")
-	defer ll.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+    ll, err := NewProj("+proj=latlong +ellps=clrk66")
+    defer ll.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	x, y, err := Transform2(ll, merc, 1, 1, DegToRad(-16), DegToRad(20.25))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%.2f %.2f", x, y)  // should print: -1495284.21 1920596.79
+    x, y, err := Transform2(ll, merc, 1, 1, DegToRad(-16), DegToRad(20.25))
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("%.2f %.2f", x, y)  // should print: -1495284.21 1920596.79
 */
 package proj
 
@@ -48,10 +48,14 @@ func NewProj(definition string) (*Proj, error) {
 	proj := Proj{opened: false}
 	proj.pj = C.pj_init_plus(cs)
 
-	err := getError()
-	if err == nil {
+	var err error = nil
+	s := C.GoString(C.get_err())
+	if s == "" {
 		proj.opened = true
+	} else {
+		err = errors.New(s)
 	}
+
 	return &proj, err
 }
 
@@ -82,14 +86,6 @@ func Transform3(srcpj, dstpj *Proj, point_count int64, point_offset int, x, y, z
 		return 0.0, 0.0, 0.0, errors.New(e)
 	}
 	return float64(C.triple_x(triple)), float64(C.triple_y(triple)), float64(C.triple_z(triple)), nil
-}
-
-func getError() error {
-	s := C.GoString(C.get_err())
-	if s == "" {
-		return nil
-	}
-	return errors.New(s)
 }
 
 func DegToRad(deg float64) float64 {
