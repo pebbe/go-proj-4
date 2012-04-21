382d81d10a5448e827d46600201e8716bee3e4dd
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-04-21 14:22:51 +0200

    formatting

--- proj/proj.go
+++ proj/proj.go
@@ -5,23 +5,23 @@ See: http://trac.osgeo.org/proj/
 
 Example usage:
 
-    merc, err := NewProj("+proj=merc +ellps=clrk66 +lat_ts=33")
-    defer merc.Close()
-    if err != nil {
-        log.Fatal(err)
-    }
+	merc, err := NewProj("+proj=merc +ellps=clrk66 +lat_ts=33")
+	defer merc.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    ll, err := NewProj("+proj=latlong +ellps=clrk66")
-    defer ll.Close()
-    if err != nil {
-        log.Fatal(err)
-    }
+	ll, err := NewProj("+proj=latlong +ellps=clrk66")
+	defer ll.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    x, y, err := Transform2(ll, merc, 1, 1, DegToRad(-16), DegToRad(20.25))
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("%.2f %.2f", x, y)  // should print: -1495284.21 1920596.79
+	x, y, err := Transform2(ll, merc, 1, 1, DegToRad(-16), DegToRad(20.25))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2f %.2f", x, y)  // should print: -1495284.21 1920596.79
 */
 package proj
 
@@ -33,7 +33,7 @@ import "C"
 
 import (
 	"errors"
-    "math"
+	"math"
 	"unsafe"
 )
 
@@ -56,32 +56,32 @@ func NewProj(definition string) (*Proj, error) {
 }
 
 func (p *Proj) Close() {
-    if p.opened {
-        C.pj_free(p.pj)
-        p.opened = false
-    }
+	if p.opened {
+		C.pj_free(p.pj)
+		p.opened = false
+	}
 }
 
 func Transform2(srcpj, dstpj *Proj, point_count int64, point_offset int, x, y float64) (float64, float64, error) {
-    if !(srcpj.opened && dstpj.opened) {
-        return 0.0, 0.0, errors.New("projection is closed")
-    }
+	if !(srcpj.opened && dstpj.opened) {
+		return 0.0, 0.0, errors.New("projection is closed")
+	}
 	triple := C.transform2(srcpj.pj, dstpj.pj, C.long(point_count), C.int(point_offset), C.double(x), C.double(y))
 	if e := C.GoString(C.triple_err(triple)); e != "" {
-        return 0.0, 0.0, errors.New(e)
+		return 0.0, 0.0, errors.New(e)
 	}
-    return float64(C.triple_x(triple)), float64(C.triple_y(triple)), nil
+	return float64(C.triple_x(triple)), float64(C.triple_y(triple)), nil
 }
 
 func Transform3(srcpj, dstpj *Proj, point_count int64, point_offset int, x, y, z float64) (float64, float64, float64, error) {
-    if !(srcpj.opened && dstpj.opened) {
-        return 0.0, 0.0, 0.0, errors.New("projection is closed")
-    }
-    triple := C.transform3(srcpj.pj, dstpj.pj, C.long(point_count), C.int(point_offset), C.double(x), C.double(y), C.double(z))
-    if e := C.GoString(C.triple_err(triple)); e != "" {
-        return 0.0, 0.0, 0.0, errors.New(e)
-    }
-    return float64(C.triple_x(triple)), float64(C.triple_y(triple)), float64(C.triple_z(triple)), nil
+	if !(srcpj.opened && dstpj.opened) {
+		return 0.0, 0.0, 0.0, errors.New("projection is closed")
+	}
+	triple := C.transform3(srcpj.pj, dstpj.pj, C.long(point_count), C.int(point_offset), C.double(x), C.double(y), C.double(z))
+	if e := C.GoString(C.triple_err(triple)); e != "" {
+		return 0.0, 0.0, 0.0, errors.New(e)
+	}
+	return float64(C.triple_x(triple)), float64(C.triple_y(triple)), float64(C.triple_z(triple)), nil
 }
 
 func getError() error {
@@ -93,9 +93,9 @@ func getError() error {
 }
 
 func DegToRad(deg float64) float64 {
-    return deg / 180.0 * math.Pi
+	return deg / 180.0 * math.Pi
 }
 
 func RadToDeg(rad float64) float64 {
-    return rad / math.Pi * 180.0
+	return rad / math.Pi * 180.0
 }
